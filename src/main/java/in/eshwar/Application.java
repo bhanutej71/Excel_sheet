6aba1687b3c6a1fb2d9e3a9e3810b485da68d4d1
bhanutej71 <bhanudornadula71@gmail.com>
2023-03-29 20:23:39 +0530

    file added

---
+++ src/main/java/in/eshwar/Application.java
@@ -0,0 +1,13 @@
+package in.eshwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
